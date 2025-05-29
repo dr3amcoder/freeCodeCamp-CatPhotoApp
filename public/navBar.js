7b9d7cd8b33872cb60afbbcc57777614ce263a93
dr3amcoder <jannalimpin@hotmail.co.uk>
2025-05-29 11:56:52 +0100

    navBar created and added to all pages

---
+++ public/navBar.js
@@ -0,0 +1,13 @@
+const navBar = 
+    `<nav class="navBar">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="catPhotos.html">Cat Photos</a></li>
+            <li><a href="catFacts.html">Cat Facts</a></li>
+            <li><a href="catProfile.html">Cat Profile</a></li>
+        </ul>
+    </nav>`
+;
+
+
+document.getElementById('navBar').innerHTML = navBar;
